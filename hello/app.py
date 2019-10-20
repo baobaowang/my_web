b11a515dfe1d92a48e35aac1c6c57a218fc66195
wangnan <1336514308@qq.com>
2019-10-20 19:15:12 +0800

    AJAX,HTTP服务端推送,web安全

--- hello/app.py
+++ hello/app.py
@@ -190,3 +190,41 @@ def redirect_back(default='hello',**kwargs):
         if is_safe_url(target):
             return redirect(target)
     return redirect(url_for(default,**kwargs))
+
+
+#AJAX
+
+from jinja2.utils import generate_lorem_ipsum
+@app.route('/post')
+def show_post():
+    post_body = generate_lorem_ipsum(n=2)
+    return '''
+<h1>A very long post</h1>
+<div class="body">%s</div>
+<button id="load">Load More</button>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script type="text/javascript">
+$(function() {
+    $('#load').click(function() {
+        $.ajax({
+            url: '/more',
+            type: 'get',
+            success: function(data){
+                $('.body').append(data);
+            }
+        })
+    })
+})
+</script>''' % post_body
+
+
+@app.route('/more')
+def load_post():
+    return generate_lorem_ipsum(n=1)
+
+
+
+
+
+
+
