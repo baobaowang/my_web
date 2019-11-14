443e930e53aae42a4d455e32600a5910dd065bac
wangnan <1336514308@qq.com>
2019-11-14 22:30:43 +0800

    邮件: 发送邮件

--- .flaskenv
+++ .flaskenv
@@ -1,3 +1,3 @@
 #这是注释
-FLASK_APP = hello/app
+FLASK_APP = email/app
 FLASK_ENV=development
--- email/app.py
+++ email/app.py
@@ -1,17 +1,19 @@
-from flask import Flask,redirect,url_for,render_template
-from flask_mail import Mail
+import os
+from flask import Flask,redirect,url_for,render_template,flash
+from flask_mail import Mail,Message
 
 app = Flask(__name__)
 
 
 #敏感信息,在env中获取
 app.config.update(
-    MAIL_SERVER = os.getenv('MAIL_SERVER'),
-    MAIL_PORT = 587,
-    MAIL_USE_TLS = True,
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME'),
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD'),
-    MAIL_DEFAULT_SENDER=('Wang ...',os.getenv('MAIL_USERNAME'))
+    SECRET_KEY = os.getenv('SECRET_KEY','secret string'),
+    MAIL_SERVER = os.getenv('MAIL_SERVER'), #发信服务器
+    MAIL_PORT = 587,    #发信端口
+    MAIL_USE_TLS = True,    #TLS加密
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME'), #服务器用户名
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD'), #密码  qq邮箱为授权码
+    MAIL_DEFAULT_SENDER=('Wang ...',os.getenv('MAIL_USERNAME'))# 发信人  使用SMTP服务时,发信人和用户名必须一样
 )
 
 mail = Mail(app)
@@ -26,16 +28,17 @@ class SubscribeForm(FlaskForm):
     submit = SubmitField('Subscribe')
 
 #通用发信函数
+#subject主题   recipients为收件人邮件地址的列表  body为正文
 def send_mail(subject,to,body):
-    message = Message(subject,recipients=[to],body = body)
-    mail.send(message)
+    message = Message(subject,recipients=[to],body = body) #邮件通过Message类构建
+    mail.send(message)          #调用maile.send()发送邮件
 
-@app.route('/subscribe',methods=['GET','POST'])
-def subscribe():
+@app.route('/',methods=['GET','POST'])
+def index():
     form = SubscribeForm()
     if form.validate_on_submit():
         email = form.email.data
-        send_mail('订阅成功',email,'你好,欢迎订阅...')
+        send_mail('订阅成功',email,'巧巧吃屁') #调用通用发信函数
         flash('提交成功')
         return redirect(url_for('index'))
     return render_template('index.html',form = form )
